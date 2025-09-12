be9f5ab726541318aef8309c77f2ce782a709f5b
JSYRD <1353097778@qq.com>
2025-09-12 18:47:51 +0800

    ref & feat: 修改为实时模式

--- main.py
+++ main.py
@@ -1,9 +1,39 @@
 import argparse
-from src.converter import converter
+import subprocess
+from functools import partial
+import sys
+import logging
+from livereload import Server
+from pathlib import Path
+
+def build(filepath: str):
+    print("🔁 Rebuilding...")
+    try:
+        result = subprocess.run(
+            [sys.executable, "build.py", filepath],
+            capture_output=True,
+            text=True
+        )
+        if result.returncode != 0:
+            logging.log(logging.WARNING, "⚠ 编译出错，但监听继续：")
+            logging.log(logging.WARNING, result.stdout)
+            logging.log(logging.WARNING, result.stderr)
+        else:
+            logging.info("✔ 编译成功")
+    except Exception as e:
+        logging.error("❌ 执行build异常， 但监听继续：", e)
+
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("filepath", help="select a Markdown file to convert", type=str)
+    parser.add_argument("filepath", help="file path to target markdown file.", type=str)
+    parser.add_argument("--host", default="0.0.0.0", help="host ip address", type=str)
+    parser.add_argument("--port", default=5500, help="port", type=int)
     args = parser.parse_args()
 
-    converter(args.filepath)
+    build(args.filepath)
+
+    server = Server()
+    server.watch(args.filepath, partial(build, args.filepath))
+    server.serve(root=f'{Path(args.filepath).resolve().parent}/dist', open_url_delay=1, host=args.host, port=args.port)
+
