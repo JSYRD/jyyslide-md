4b5be978968a17e7ad16c155b72d7d81316535ea
JSYRD <61402085+JSYRD@users.noreply.github.com>
2026-05-14 15:54:18 +0800

    feat: 去除日志中的emoji
    Signed-off-by: JSYRD <61402085+JSYRD@users.noreply.github.com>

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ from livereload import Server
 from pathlib import Path
 
 def build(filepath: str):
-    print("🔁 Rebuilding...")
+    logging.info("Rebuilding...")
     try:
         result = subprocess.run(
             [sys.executable, "build.py", filepath],
@@ -15,16 +15,16 @@ def build(filepath: str):
             text=True
         )
         if result.returncode != 0:
-            logging.log(logging.WARNING, "⚠ 编译出错，但监听继续：")
+            logging.log(logging.WARNING, "编译出错，但监听继续：")
             logging.log(logging.WARNING, result.stdout)
             logging.log(logging.WARNING, result.stderr)
         else:
-            logging.info("✔ 编译成功")
+            logging.info("编译成功")
     except Exception as e:
-        logging.error("❌ 执行build异常， 但监听继续：", e)
+        logging.error("执行build异常， 但监听继续：", e)
 
 
-if __name__ == "__main__":
+def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("filepath", help="file path to target markdown file.", type=str)
     parser.add_argument("--host", default="0.0.0.0", help="host ip address", type=str)
@@ -37,3 +37,7 @@ if __name__ == "__main__":
     server.watch(args.filepath, partial(build, args.filepath))
     server.serve(root=f'{Path(args.filepath).resolve().parent}/dist', open_url_delay=1, host=args.host, port=args.port)
 
+
+
+if __name__ == "__main__":
+    main()
